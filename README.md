cbb1c1f51c8846ac71692ebdf6e9e6b0bda426d7
JCtech <ocjustc@gmail.com>
2024-02-22 00:45:50 +0100

    Learn to create object constructor. Learn to add object instance Learn to use event.preventDefault().

---
+++ README.md
@@ -0,0 +1,5 @@
+This project is about creating a library fill with books.
+The aim is to add a book with some details about the book like title, author, number of pages and if the book have been read or not.
+There will be an option to  delete books and edith there read status.
+With this project i will learn how to create and add items with details or descriptions that can be reused in part or whole.
+Even though there may be other methods of doing this, I will be using objects in Javascript for this project. This will be my first project after learning Javascript Objects. It may not look great but i will give it my best.
---
+++ script.js
@@ -0,0 +1,148 @@
+const myLibrary = [];
+
+function addBookToLibrary(book) {
+  if (!myLibrary.includes(book)) {
+  myLibrary.push(book)
+  }
+};
+
+function Book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.status = false;
+}
+
+Book.prototype.getStatus = function() {
+
+  return this.status === true ? 'Read' : 'Unread';
+}
+
+let main = document.querySelector('.main');
+
+function displayBook(book) {
+
+  main.innerHTML = '';
+
+  book.forEach((element, index) => {
+    const books = document.createElement('div');
+
+    const bookName = document.createElement('div');
+    bookName.textContent = 'Tittle: ' + element.name;
+    books.appendChild(bookName);
+
+    const bookAuthor = document.createElement('div');
+    bookAuthor.textContent = 'Author: ' + element.author;
+    books.appendChild(bookAuthor);
+
+    const bookPages = document.createElement('div');
+    bookPages.textContent = 'Pages: ' + element.pages;
+    books.appendChild(bookPages);
+
+    const bookStatus = document.createElement('div');
+    bookStatus.textContent = 'Status: ' + element.getStatus();
+    books.appendChild(bookStatus);
+
+    const deleteBook = document.createElement('button');
+    deleteBook.classList.add('deleteButton');
+    deleteBook.textContent = 'Delete';
+    books.appendChild(deleteBook);
+
+    const changeStatus = document.createElement('button');
+    changeStatus.classList.add('changeButton');
+    changeStatus.textContent = 'Change Status';
+    books.appendChild(changeStatus);
+
+    let status = myLibrary[index].status
+
+    changeStatus.addEventListener('click', () => {
+      status ? status = false : status = true;
+      myLibrary[index].status = status
+    displayBook(myLibrary)
+    })
+
+
+
+
+
+    books.id = index;
+    books.classList.add('book');
+
+    deleteBook.addEventListener('click', () => removeBook(index))
+
+    
+
+    main.appendChild(books);
+  })
+}
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+
+  displayBook(myLibrary)
+}
+
+function updateStatus(array,property, value) {
+  for (let i = 0; i < objects.length; i++){
+    if (array[i][property] === value){
+      return i;
+    }
+  }
+  return -1
+
+
+}
+
+const modal = document.querySelector('.modal');
+
+const addBook = document.querySelector('.addBook');
+
+addBook.addEventListener('click', () => {
+  modal.showModal();
+})
+
+const cancel = document.querySelector('.cancelButton');
+cancel.addEventListener('click', () => {
+  modal.close();
+  form.reset();
+});
+
+const form = document.querySelector('.modalForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const inputName = document.querySelector('.inputName').value;
+  
+  const inputAuthor = document.querySelector('.inputAuthor').value;
+  
+  const inputPage = document.querySelector('.inputPage').value;
+
+  const inputStatus = document.querySelector('.inputStatus').checked;
+
+  const newBook = new Book(inputName, inputAuthor, inputPage);
+
+
+  newBook.status = inputStatus;
+
+  addBookToLibrary(newBook);
+
+  displayBook(myLibrary)
+  
+  form.reset();
+  modal.close();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
